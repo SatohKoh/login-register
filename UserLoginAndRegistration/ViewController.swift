263bda21b4403ef1f1d7042ee8dade2b513ac2c3
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2019-01-19 13:14:49 +0900

    Initial Commit

---
+++ UserLoginAndRegistration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by 佐藤恒 on 2019/01/19.
+//  Copyright © 2019年 佐藤恒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UserLoginAndRegistrationTests/UserLoginAndRegistrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  UserLoginAndRegistrationTests.swift
+//  UserLoginAndRegistrationTests
+//
+//  Created by 佐藤恒 on 2019/01/19.
+//  Copyright © 2019年 佐藤恒. All rights reserved.
+//
+
+import XCTest
+@testable import UserLoginAndRegistration
+
+class UserLoginAndRegistrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
